a56bfbc2684528822a770de3c4d2fc7e0f457801
Oliver Liam Kelly <oliverliamkelly@gmail.com>
2022-04-19 20:24:24 +1000

    immitating the mini project for the post method for the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,12 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jatedb = await openDB('jate', 1);
+  const trs = jatedb.transaction('jate', 'readwrite');
+  const store = trs.objectStore('jate');
+  const request = store.add({}); 
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
